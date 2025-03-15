9f98b1db98dae0dd77bb92180828e8a23787b66f
kim9842 <61659301+kim9842@users.noreply.github.com>
2025-03-15 23:39:38 +0200

    8

--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,6 @@
-class StringBuilder {
-#value;
-constructor(initialValue) {
-    this.#value = initialValue;
-}
-getValue() {
-    return this.#value;
-}
-padEnd(str) {
-    this.#value = this.#value + str;
-}
-padStart(str) {
-    this.#value = str + this.#value;
-}
-padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-}
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
 
-}
-
-
-
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
-
+input.addEventListener("input", (event) => {
+  output.textContent = event.target.value.trim() || "Anonymous";
+});
